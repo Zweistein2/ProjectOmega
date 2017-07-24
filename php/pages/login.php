cb3dbd5de7f71025e5073405fd1161b69f523ee9
zunix <zunix.pdc@gmail.com>
2017-07-24 21:13:58 +0200

    #16 Login Maske angepasst. Sidebar von der Login Seite entfernt.

--- php/pages/login.php
+++ php/pages/login.php
@@ -7,35 +7,28 @@
     <html>
     <head>
     <meta charset="utf-8"/>
-        <?php include("../template/head.template.php"); ?>
-        <link href="../../css/login.css" rel="stylesheet">
-    </head>
-    <body>
-        <?php include("../template/sidebar.template.php"); ?>
-        <div class="container">
-            <div class="container">
-
-                <div class="card card-container">
-                    <h2 class='login_title text-center'>Anmeldung</h2>
-                    <hr>
-
-                    <form class="form-signin">
-                        <span id="reauth-email" class="reauth-email"></span>
-                        <p class="input_title">Email</p>
-                        <input type="text" id="inputEmail" class="login_box" placeholder="verwaltung@b3-fuerth.de" required autofocus>
-                        <p class="input_title">Passwort</p>
-                        <input type="password" id="inputPassword" class="login_box" placeholder="******" required>
-                        <div id="remember" class="checkbox">
-                            <label>
-
-                            </label>
-                        </div>
-                        <button class="btn btn-lg btn-primary" type="submit">Login</button>
-                    </form><!-- /form -->
-                </div><!-- /card-container -->
-            </div><!-- /container -->
+    <?php include("../template/head.template.php"); ?>
+    <link href="../../css/login.css" rel="stylesheet">
+</head>
+<body>
+<div class="container">
+    <div class="container">
+        <div class="card card-container">
+            <h2 class='login_title text-center'>Anmeldung</h2>
+            <hr>
+            <form class="form-signin">
+                <span id="reauth-email" class="reauth-email"></span>
+                <p class="input_title">Email</p>
+                <input type="text" id="inputEmail" class="login_box" placeholder="verwaltung@b3-fuerth.de" required
+                       autofocus>
+                <p class="input_title">Passwort</p>
+                <input type="password" id="inputPassword" class="login_box" placeholder="******" required>
+                <button class="btn btn-lg btn-primary" type="submit">anmelden</button>
+            </form>
         </div>
-    </body>
+    </div>
+</div>
+</body>
 </html>
 
 
