d5f6e53c26605ea4e318eb510d2823a02daec25b
Fabian Karolat <fkarolat@yahoo.com>
2017-07-24 17:38:35 +0200

    #21 fixed merge-conflict

--- php/login.php
+++ php/login.php
@@ -10,7 +10,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="../js/bootstrap.js"></script>
     <link href="../css/bootstrap.css" rel="stylesheet">
-    <link href="../css/style.css" rel="stylesheet">
+    <link href="../css/navbar.css" rel="stylesheet">
+    <link href="../css/login.css" rel="stylesheet">
     <?php
     //include("template/head.template.php");
     ?>
