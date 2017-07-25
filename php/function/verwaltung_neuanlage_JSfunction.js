8c0f9629985b0b348702854068cd0ca1a71de303
DanielLinz <linz.daniellauf@gmail.com>
2017-07-25 10:14:35 +0200

    #26

---
+++ php/function/verwaltung_neuanlage_JSfunction.js
@@ -0,0 +1,46 @@
+
+//Füllt die Tabelle mit Daten die durch die Page Variable eingegränzt wurde
+function renderTable(page) {
+    $("tbody").html("");
+    var viewModel = javascript_array;
+    for (var i = (page - 1) * 10; i < page * 10; i++) {
+        var htmlString = "" +
+            "<tr>\n" +
+            "      <td>" + viewModel[i]["ID"] + "</td>\n" +
+            "      <td>" + viewModel[i]["Hersteller"] + "</td>\n" +
+            "      <td>" + viewModel[i]["CPU"] + "</td>\n" +
+            "      <td>\n" +
+            "         <div class=\"checkbox\">\n" +
+            "            <input type=\"checkbox\" value=\"\">\n" +
+            "         </div>\n" +
+            "      </td>\n" +
+            "</tr>";
+        $("tbody").append(htmlString, null);
+        $("#pageCounter").html("<a href=\"#\">" + page + "</a>")
+    }
+}
+
+function Init() {
+    var page = 1;
+    renderTable(page);
+
+    $("#prev").click(function () {
+        page--;
+        renderTable(page);
+    });
+
+    $("#next").click(function () {
+        page++;
+        renderTable(page);
+    });
+
+    $("#nextBig").click(function () {
+        page += 10;
+        renderTable(page);
+    });
+
+    $("#prevBig").click(function () {
+        page -= 10;
+        renderTable(page);
+    });
+}
