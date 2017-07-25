12133af427f178b0c948399d261f93048be18758
Fabian Karolat <fkarolat@yahoo.com>
2017-07-25 10:55:10 +0200

    #26 Database-stuff

--- js/verwaltung_JSfunction.js
+++ js/verwaltung_JSfunction.js
@@ -1,19 +1,19 @@
 
-//Füllt die Tabelle mit Daten die durch die Page Variable eingegränzt wurde
+//Füllt die Tabelle mit Daten die durch die Page Variable eingegrenzt wurde
 function renderTable(page) {
     $("tbody").html("");
     var viewModel = javascript_array;
     for (var i = (page - 1) * 10; i < page * 10; i++) {
         var htmlString = "" +
             "<tr>\n" +
-            "      <td>" + viewModel[i]["ID"] + "</td>\n" +
-            "      <td>" + viewModel[i]["Hersteller"] + "</td>\n" +
-            "      <td>" + viewModel[i]["CPU"] + "</td>\n" +
-            "      <td>\n" +
-            "         <div class=\"checkbox\">\n" +
-            "            <input type=\"checkbox\" value=\"\">\n" +
-            "         </div>\n" +
-            "      </td>\n" +
+            "<td>" + viewModel[i]["ID"] + "</td>\n" +
+            "<td>" + viewModel[i]["Hersteller"] + "</td>\n" +
+            "<td>" + viewModel[i]["CPU"] + "</td>\n" +
+            "<td>\n" +
+            "<div class=\"checkbox\">\n" +
+            "<input type=\"checkbox\" value=\"\">\n" +
+            "</div>\n" +
+            "</td>\n" +
             "</tr>";
         $("tbody").append(htmlString, null);
         $("#pageCounter").html("<a href=\"#\">" + page + "</a>")
