527d7ff3d6062fe658352758216c14226b93ea60
nreke <maria.mk.kern@gmail.com>
2017-07-25 13:43:49 +0200

    #25: insert

--- php/database/stammdaten_sql.php
+++ php/database/stammdaten_sql.php
@@ -1,29 +1,66 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mkern
- * Date: 25.07.2017
- * Time: 09:34
- */
+include_once('database.php');
 
-//TODO
-$connection = mysqli_connect('192.168.20.1', 'dev', 'dev', 'softwareTest');
+define(HARDWARE, 'hardware');
+define(HARDWARE_ATTRIBUTES, 'hardware_hat_attribute');
+define(HARDWARE_KINDS, 'hardwarearten');
+define(SUPPLIERS, 'lieferant');
+define(ROOMS, 'raeume');
+
+//--hardware
+define(H_ID, 'h_id');
+define(H_ROOM_ID, 'raeume_r_id');
+define(H_SUPPLIER_ID, 'lieferant_l_id');
+define(H_STATUS, 'h_status');
+define(H_NAME, 'h_name');
+define(H_DESC, 'h_bez');
+define(H_BUY_DATE, 'h_einkaufsdatum');
+define(H_WARRANTY, 'h_gewaehrleistungsdauer');
+define(H_NOTE, 'h_notiz');
+define(H_DEV, 'h_hersteller');
+define(H_ATTR_ID, 'hardwarearten_ha_id');
+
+//--lieferant
+define(L_ID, 'l_id');
+define(L_COMPANY_NAME, 'l_firmenname');
+define(L_STREET, 'l_strasse');
+define(L_PLZ, 'l_plz');
+define(L_TOWN, 'l_ort');
+define(L_TEL, 'l_tel');
+define(L_MOBILE, 'l_mobil');
+define(L_FAX, 'l_fax');
+define(L_EMAIL, 'l_email');
+
+//--räume
+define(R_ID, 'r_id');
+define(R_NR, 'r_nr');
+define(R_DESC, 'r_bezeichnung');
+define(R_NOTE, 'r_notiz');
+
+//--arten
+define(K_ID, 'ha_id');
+define(K_NAME, 'ha_hardwareart');
+
+//--attribute
+define(A_ID, 'hat_id');
+define(A_DESC, 'hat_bezeichnung');
+
 
 function getComponents(){
     global $connection;
-    $query = 'SELECT * FROM komponenten';
+    $query = 'SELECT * FROM '.HARDWARE;
     return mysqli_query($connection, $query);
 }
 function getComponentsPlus(){
     global $connection;
-    $query = 'SElECT comp.*, rooms.r_nr AS raumnummer, supp.l_firmenname AS firmenname FROM komponenten AS comp '
-            .' INNER JOIN raeume AS rooms ON rooms.r_id = comp.raeume_r_id '
-            .' INNER JOIN lieferant AS supp ON supp.l_id = comp.lieferant_l_id';
+    $query = 'SElECT comp.*, rooms.'.R_NR.' AS raumnummer, supp.'.L_COMPANY_NAME.' AS firmenname FROM '.HARDWARE.' AS comp '
+            .' INNER JOIN '.ROOMS.' AS rooms ON rooms.'.R_ID.' = comp.'.H_R_ID.' '
+            .' INNER JOIN '.SUPPLIERS.' AS supp ON supp.'.L_ID.' = comp.'.H_L_ID;
     return mysqli_query($connection, $query);
 }
 function getAttributesByKaID($ka_id){
     global $connection;
-    $query = 'SELECT attr.* FROM komponentenattribute AS attr '
+    $query = 'SELECT attr.* FROM '.ATTRIBUTES.' AS attr '
             .' INNER JOIN wird_beschrieben_durch AS bes ON attr.kat_id = bes.komponentenattributte_kat_id '
             .'WHERE bes.komponenten_ka_id = ' . $ka_id;
     return mysqli_query($connection, $query);
@@ -37,34 +74,28 @@ function getEntriesByTable($tabname){
 function getOneByTableAndID($tabname, $id){
     global $connection;
     $prim = [
-        'komponenten' => 'k_id',
-        'raeume' => 'r_id',
-        'lieferant' => 'l_id',
-        'komponentenarten' => 'ka_id',
-        'komponentenattribute' => 'kat_id'
+        HARDWARE => H_ID,
+        ROOMS => R_ID,
+        SUPPLIERS => L_ID,
+        KINDS => K_ID,
+        ATTRIBUTES => A_ID
     ];
     $query = 'SELECT * FROM '.$tabname.' WHERE '.$prim[$tabname].'='.$id;
     $results = mysqli_query($connection, $query);
     return mysqli_fetch_assoc($results);
 }
+
 function getRooms(){
     global $connection;
-    $query = 'SELECT * FROM raeume';
+    $query = 'SELECT * FROM '.ROOMS;
     return mysqli_query($connection, $query);
 }
 function getSuppliers(){
     global $connection;
-    $query = 'SELECT * FROM lieferant';
+    $query = 'SELECT * FROM '.SUPPLIERS;
     return mysqli_query($connection, $query);
 }
-/*
-function getComponentByID($k_id){
-    global $connection;
-    $query = 'SELECT * FROM komponenten WHERE k_id = ' . $k_id;
-    $result = mysqli_query($connection, $query);
-    return mysqli_fetch_assoc($result);
-}
-*/
+
 function copyComponent($k_id, $count){
     global $connection;
     if($count < 1) return;
@@ -129,5 +160,44 @@ function deleteEntryByTableAndID($tabname, $id){
         default:
             break;
     }
+}
+function insertComponent($data){
+    global $connection;
+    $r_id = $data['raeume_id'];
+    $l_id = $data['lieferant_l_id'];
+    $k_edate = mysqli_real_escape_string($data['k_einkaufsdatum']);
+    $k_gwd = $data['k_gewaehrleistungsdauer'];
+    $k_notiz = mysqli_real_escape_string($data['k_notiz']);
+    $supp = mysqli_real_escape_string($data['k_hersteller']);
+    $kind = $data['komponentenarten_ka_id'];
+
+    $query = 'INSERT INTO komponenten '
+        . '(raeume_id, lieferant_l_id, k_einkaufsdatum, k_gewaehrleistungsdauer, k_notiz, k_hersteller, k_komponentenarten_ka_id) '
+        . ' VALUES ('.$r_id.','.$l_id.','.$k_edate.','.$k_gwd.','.$k_notiz.','.$supp.','.$kind.')';
+    mysqli_query($connection, $query);
+}
+function insertIntoTable($tabname, $data){
+    global $connection;
+    $keys = array_keys($data);
+    $count = count($data);
+    $query = 'INSERT INTO '.$tabname.' (';
+    for($i = 0; $i < $count; $i){
+        $query .= $keys[$i];
+        if($i == $count-1){
+            $query .= ')';
+        }else{
+            $query .= ',';
+        }
+    }
+    $query .= ' VALUES (';
+    for($i = 0; $i < $count; $i++){
+        $query .= '"' . mysqli_real_escape_string($data[$keys[$i]]) . '"';
+        if($i == $count-1){
+            $query .= ')';
+        }else{
+            $query .= ',';
+        }
+    }
+    mysqli_query($connection, $query);
 }
 
