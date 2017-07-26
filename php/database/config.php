1db89eeb11b6ae1ebb18a0859701cafbc460ecfc
cs8555 <cs8555@consorsbank.de>
2017-07-26 09:10:34 +0200

    #25

--- php/database/config.php
+++ php/database/config.php
@@ -3,12 +3,12 @@
 require_once("cryption.php");
 
 $file = "../database/pass.enc";
-$db = "nutzer";
+$db = "softwareTest";
 $user = "dev";
 $pass = "dev";
 $ip = "192.168.20.1";
 
-$db_userDatabase = "nutzer";
+$db_userDatabase = "softwareTest";
 $user_userDatabase = "dev";
 $pass_userDatabase = "dev";
 $ip_userDatabase = "192.168.20.1";
