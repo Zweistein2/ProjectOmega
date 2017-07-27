884bdc18209ee5b64a7237de54d359ee18ef669f
Fabian Karolat <fkarolat@yahoo.com>
2017-07-27 12:35:04 +0200

    Merge branch 'master' into feature_stammdaten
    Fixed total brainfuck with js

--- php/pages/help.php
+++ php/pages/help.php
@@ -4,40 +4,50 @@
  * Date: 26.07.2017
  * Time: 8:24
  -->
+
+<?php
+require_once("../authentication/auth_filter.php");
+checkForMinAccess("Lehrer");
+?>
+
 <html>
-    <head>
-        <title>Hilfeseite</title>
-        <?php
-        include_once("../template/head.template.php");
-        ?>
-    </head>
-    <body>
-        <?php include_once("../template/sidebar.template.php"); ?>
-        <div class="container">
-            <h2>Hilfeseite</h2>
-            <div class="row">
-                <div class="col-md-4">
-                    <div class="panel panel-info">
-                        <div class="panel-heading">
-                            <p>Brauchen sie Hilfe?</p>
-                        </div>
-                        <div class="panel-body">
-                            <p>hier werden sie geholfen!</p>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="jumbotron">
-                                <h1>Wir brauchen Hilfe!</h1>
-                                <p>weil wir keinen Käsetoast mehr haben</p>
-                                <p><a class="btn btn-primary btn-lg" href="#" role="button">*klick*</a></p>
-                                <div class="alert alert-warning alert-dismissible" role="alert">
-                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                    <strong>Warning!</strong> Better check yourself, you're not looking too good.
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+<head>
+    <title>Hilfeseite</title>
+    <?php
+    include_once("../template/head.template.php");
+    ?>
+</head>
+<body>
+<?php include_once("../template/sidebar.template.php"); ?>
+<div class="container">
+    <h2>Hilfeseite</h2>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="panel panel-info">
+                <div class="panel-heading">
+                    <p>Brauchen sie Hilfe?</p>
+                </div>
+                <div class="panel-body">
+                    <p>hier werden sie geholfen!</p>
+                </div>
+                <div class="panel-footer">
+                    <p>Fußzeile.</p>
                 </div>
             </div>
         </div>
-    </body>
+
+        <div class="col-md-6">
+            <div class="jumbotron">
+                <h1>Wir brauchen Hilfe!</h1>
+                <p>weil wir keinen Käsetoast mehr haben</p>
+                <p><a class="btn btn-primary btn-lg" href="#" role="button">*klick*</a></p>
+                <div class="alert alert-warning alert-dismissible" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    <strong>Warning!</strong> Better check yourself, you're not looking too good.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
 </html>
